57e3aab3f94422cec73dbc7288cb7357ef62ad02
ВашÐÐÐDenis Kudrik <deniskudrik@gmail.com>
2021-11-28 19:42:23 +0300

    Formatting issues fixing

--- homework6/tests/test_oop_2.py
+++ homework6/tests/test_oop_2.py
@@ -1,7 +1,8 @@
 import unittest
+
 import pytest
 
-from hw.oop_2 import DeadlineError, Homework, HomeworkResult, Student, Teacher
+from hw.oop_2 import Homework, HomeworkResult, Student, Teacher
 
 
 def test_oop_2():
@@ -15,13 +16,15 @@ def test_oop_2():
     docs_hw = opp_teacher.create_homework("Read docs", 5)
 
     result_1 = good_student.do_homework(oop_hw, "I have done this hw")
-    assert result_1.solution ==  "I have done this hw"
+    assert result_1.solution == "I have done this hw"
     result_2 = good_student.do_homework(docs_hw, "I have done this hw too")
     assert result_2.solution == "I have done this hw too"
     result_3 = lazy_student.do_homework(docs_hw, "done")
     assert result_3.solution == "done"
 
-    with unittest.TestCase.assertRaises(unittest.TestCase, expected_exception=AttributeError):
+    with unittest.TestCase.assertRaises(
+        unittest.TestCase, expected_exception=AttributeError
+    ):
         HomeworkResult(good_student, "fff", "Solution")
 
     assert opp_teacher.check_homework(result_1) is True
@@ -35,6 +38,9 @@ def test_oop_2():
     opp_teacher.check_homework(result_3)
     assert len(Teacher.homework_done) == 2
 
-    assert str(Teacher.homework_done[oop_hw]) == "{HomeworkResult instance. Solution: I have done this hw}"
+    assert (
+        str(Teacher.homework_done[oop_hw])
+        == "{HomeworkResult instance. Solution: I have done this hw}"
+    )
     assert Teacher.reset_results() is None
     assert len(Teacher.homework_done) == 0
