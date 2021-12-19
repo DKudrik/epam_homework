83bb7d74e68ffba9a9a2059d4dbb1a50c4f3e851
ВашÐÐÐDenis Kudrik <deniskudrik@gmail.com>
2021-12-19 09:52:36 +0300

    Fixed found issues

--- homework8/hw/task2.py
+++ homework8/hw/task2.py
@@ -9,26 +9,29 @@ class TableData:
             self.__cursor = conn.cursor()
 
     def __len__(self):
-        query = f"SELECT * FROM {self.__tb_name}"
-        self.__cursor.execute(query)
-        rows = self.__cursor.fetchall()
-        return len(rows)
+        self.__cursor.execute(f"SELECT * FROM {self.__tb_name}")
+        data = []
+        while row := self.__cursor.fetchone():
+            data.append(row)
+        return len(data)
 
     def __iter__(self):
-        query = f"SELECT * FROM {self.__tb_name}"
-        self.__cursor.execute(query)
-        data = self.__cursor.fetchall()
+        self.__cursor.execute(f"SELECT * FROM {self.__tb_name}")
+        data = []
+        while row := self.__cursor.fetchone():
+            data.append(row)
         names = list(map(lambda x: x[0], self.__cursor.description))
-        output = [dict(zip(names, data[index]))
-                  for index, name in enumerate(names)]
+        output = [dict(zip(names, data[index])) for index, name in enumerate(names)]
         return iter(output)
 
     def __getitem__(self, key):
-        query = f"SELECT * FROM {self.__tb_name} WHERE name = '{key}'"
-        self.__cursor.execute(query)
+        self.__cursor.execute(
+            f"SELECT * FROM {self.__tb_name} WHERE name=:name", {"name": key}
+        )
         data = self.__cursor.fetchone()
         return data
 
     def __contains__(self, item):
-        query = f"SELECT * FROM {self.__tb_name}  WHERE name = '{item}'"
-        return self.__cursor.execute(query).fetchone()
+        return self.__cursor.execute(
+            f"SELECT * FROM {self.__tb_name}  WHERE name=:name", {"name": item}
+        ).fetchone()
